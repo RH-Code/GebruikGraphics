5d161145a78e83487c5d5d1ff02afae7be3b10b7
Ruben H. <rhdesign.design@gmail.com>
2013-10-11 13:03:47 +0200

    Oef. Graphics

---
+++ T-vorm tekenen/T-vorm tekenen/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace T_vorm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTeken = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTeken
+            // 
+            this.btnTeken.Location = new System.Drawing.Point(12, 12);
+            this.btnTeken.Name = "btnTeken";
+            this.btnTeken.Size = new System.Drawing.Size(75, 23);
+            this.btnTeken.TabIndex = 0;
+            this.btnTeken.Text = "Teken";
+            this.btnTeken.UseVisualStyleBackColor = true;
+            this.btnTeken.Click += new System.EventHandler(this.btnTeken_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(107, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 324);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnTeken);
+            this.Name = "Form1";
+            this.Text = "T-vorm tekenen";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTeken;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ T-vorm tekenen/T-vorm tekenen/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace T_vorm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTeken_Click(object sender, EventArgs e)
+        {
+            Graphics paper;
+            paper = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.Blue);
+            
+            /*The T-shape with 8 lines.*/
+            paper.DrawLine(pen, 10, 10, 280, 10);
+            paper.DrawLine(pen, 10, 10, 10, 70);
+            paper.DrawLine(pen, 280, 10, 280, 70);
+            paper.DrawLine(pen, 10, 70, 105, 70);
+            paper.DrawLine(pen, 175, 70, 280, 70);
+            paper.DrawLine(pen, 105, 70, 105, 280);
+            paper.DrawLine(pen, 175, 70, 175, 280);
+            paper.DrawLine(pen, 105, 280, 175, 280);
+
+            /*The T-shape with 2 rectangles*/
+            //paper.DrawRectangle(pen, 10, 10, 280, 70);
+            //paper.DrawRectangle(pen, 105, 80, 80, 210);
+        }
+    }
+}
